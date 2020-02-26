59342898c8ebb7bc50aa504472b7991b82c4f671
Riley <rilmaher@umich.edu>
2020-02-26 16:14:57 -0500

    initial setup

---
+++ playlist_creator.py
@@ -0,0 +1,18 @@
+class CreatePLaylist:
+
+    def get_youtube_permission(self):
+        pass
+
+    def get_liked_video(self):
+        pass
+
+    def create_playlist(self):
+        pass
+
+    def get_spotify_url(self):
+        pass
+
+    def add_song_to_playlist(self):
+        pass
+
+    
