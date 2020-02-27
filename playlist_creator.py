331f650fc4eb2e16ff0c60fe4b3acea194054f2f
Riley <rilmaher@umich.edu>
2020-02-27 13:54:27 -0500

    youtube client function

--- playlist_creator.py
+++ playlist_creator.py
@@ -2,6 +2,10 @@ import json
 import requests
 import os
 
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
 from secrets import spotify_client_id, spotify_secret_token
 
 class CreatePLaylist:
@@ -9,9 +13,27 @@ class CreatePLaylist:
     def __init__(self):
         self.user_id = spotify_client_id
         self.spotify_token = spotify_secret_token
+        self.youtube_client = self.get_youtube_permission()
 
     def get_youtube_permission(self):
-        pass
+        """ Log Into Youtube, Copied from Youtube Data API """
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+        api_service_name = "youtube"
+        api_version = "v3"
+        client_secrets_file = "client_secret.json"
+
+        # Get credentials and create an API client
+        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+        credentials = flow.run_console()
+
+        # from the Youtube DATA API
+        youtube_client = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
+
+        return youtube_client
 
     def get_liked_video(self):
         pass
