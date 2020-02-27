ab93fc6fa2fd227f10a4bf16b6a3a7e42af8b180
Riley <rilmaher@umich.edu>
2020-02-26 22:06:23 -0500

    setting up create_playlist function

--- playlist_creator.py
+++ playlist_creator.py
@@ -1,4 +1,12 @@
+import json
+import requests
+from secrets import spotify_client_id, spotify_secret_token
+
 class CreatePLaylist:
+
+    def __init__(self):
+        self.user_id = spotify_client_id
+        self.spotify_token = spotify_secret_token
 
     def get_youtube_permission(self):
         pass
@@ -7,9 +15,28 @@ class CreatePLaylist:
         pass
 
     def create_playlist(self):
-        pass
+        
+        request_body = json.dumps({
+            'name':'Liked YouTube Videos',
+            'desription':'Liked YouTube Videos',
+            'public':True
+        })
 
-    def get_spotify_url(self):
+        query = "http://api.spotify.com/v1/users/{}/playlists".format(self.user_id)
+        response = requests.post(
+            query,
+            data=requests.body,
+            headers={
+                'Content-Type':'application/json',
+                'Authorization':'Bearer {}'.format()
+            }
+        )
+
+        response_json = response.json()
+
+        return response_json('id')
+
+    def get_spotify_uri(self, track, artist):
         pass
 
     def add_song_to_playlist(self):
---
+++ secrets.py
@@ -0,0 +1,2 @@
+spotify_client_id = '92504e91a43143f28522a88b9d2b45f8'
+spotify_secret_token = '8a7ccd0315fc44528840c95f4b695e32'
